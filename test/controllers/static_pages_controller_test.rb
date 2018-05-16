64086356d0bfa5001725e47580f2da441cf51538
Dounx <imdounx@gmail.com>
2018-05-16 13:06:45 +0800

    Implement basic login

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   def setup
-  @base_title = "Ruby on Rails Tutorial Sample App"
+  @base_title = "Sample App"
   end
   
   test "should get home" do
   get root_path
   assert_response :success
-  assert_select "title", "Ruby on Rails Tutorial Sample App"
+  assert_select "title", "Sample App"
   end
 
   test "should get help" do
