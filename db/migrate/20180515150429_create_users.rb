bb82a078b102e14b1487cdc36624c26477173c14
Dounx <imdounx@gmail.com>
2018-05-16 11:39:54 +0800

    Finish user signup

--- db/migrate/20180515150429_create_users.rb
+++ db/migrate/20180515150429_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
+  create_table :users do |t|
+    t.string :name
+    t.string :email
 
-      t.timestamps
-    end
+    t.timestamps
+  end
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get signup_path
-    assert_response :success
+  get signup_path
+  assert_response :success
   end
 
 end
